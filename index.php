b2aecd3c2f1e8a12d29afbe45f262de97b9c33e1
Vergil Kelley <verg_1@yahoo.com>
2020-07-25 14:41:35 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,204 @@
+<?php
+  require 'header.php';
+?>
+    <section class="hero">
+      <div class="hero-overlay">HERO overlay</div>
+      <img src="images/bikeHero.jpg" alt="man riding bike" />
+      <div id="bikes-hero-text">
+        <h2>bikes for everyone</h2>
+        <p>Men Women Children Sport or Leisure!</p>
+        <a href="#bikes">Search Bikes</a>
+      </div>
+    </section>
+    <div id="bikes"></div>
+    <section  class="main-section">
+     <div class="bike-row-container">
+       <div class="bike-row-header">
+          <h2>Buy T-REKS</h2>
+          <p>
+            Trek is the world leader in mountain bike technology. No surprise that
+            our mountain bikes are the most technologically advanced on the
+            market. Here, innovations are not limited to only the highest-end MTB
+            models. Every Trek mountain bike is loaded with features and details
+            that will make any ride, on any trail, better.
+          </p>
+        </div>
+        <div class="bike-img-container bike-img-container1">
+          <img class="cart-item-img" src="images/trek1.jpg" alt="bike" />
+          <h2 class="item-title">City Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$599</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container2">
+          <img class="cart-item-img" src="images/trek3.jpg" alt="bike" />
+          <h2 class="item-title">Trail Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$799</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container3">
+          <img class="cart-item-img" src="images/trek5.jpg" alt="bike" />
+          <h2 class="item-title">Mountain Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$899</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container4">
+          <img class="cart-item-img" class="item-image" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Leisuer Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$999</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container5">
+          <img class="cart-item-img" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Sport Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$1299</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container6">
+          <img class="cart-item-img" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Trac Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$1999</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container7">
+          <img class="cart-item-img" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Mobile Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$2499</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container8">
+          <img class="cart-item-img" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Paris Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$3999</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+        <div class="bike-img-container bike-img-container9">
+          <img class="cart-item-img" src="images/trek6.jpg" alt="bike" />
+          <h2 class="item-title">Mid-line Trek</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ipsa
+            ad, impedit beatae eligendi rem quaerat deleniti facilis omnis
+            obcaecati.
+          </p>
+          <h3 class="item-price">$9999</h3>
+          <div class="addTo-cartBtn-Container">
+            <button class="btn btn-addToCart">Add To Cart</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="cart-section">
+      <h2 class="cart-section-header">CART</h2>
+      <div class="cart-items-header">
+            <span class="cart-item cart-header cart-column">ITEM</span>
+        <span class="cart-price-title cart-header cart-column">PRICE</span>
+        <span class="cart-quantity-title cart-header cart-column">QUANTITY</span>
+      </div>
+      <div class="cart-items">
+        <!--<div class="cart-row">
+          <div class="cart-item cart-column">
+            <img class="cart-item-image" src="images/trek1.jpg" width="100" height="100">
+            <span class="cart-item-title">Mountain Trek</span>
+        </div>
+        <span class="cart-price cart-column">$899</span>
+        <div class="cart-quantity cart-column">
+          <input class="cart-quantity-input" type="number" value="1">
+          <button class="btn btn-danger" type="button">REMOVE</div>
+      </div>
+        <div class="cart-row">
+          <div class="cart-item cart-column">
+            <img class="cart-item-image" src="images/trek1.jpg" width="100" height="100">
+            <span class="cart-item-title">Mountain Trek</span>
+        </div>
+        <span class="cart-price cart-column">$899</span>
+        <div class="cart-quantity cart-column">
+          <input class="cart-quantity-input" type="number" value="1">
+          <button class="btn btn-danger" type="button">REMOVE</div>
+      </div>-->
+      <div class="cart-items"></div>
+      </div>
+      <div class="cart-total">
+        <strong class="cart-total-title">Total</strong>
+        <span class="cart-total-price">$0</span>
+      </div>
+      <button class="btn btn-primary btn-purchase" type="button">
+        PURCHASE
+      </button>
+    </section>
+
+    <section id="large-cards">
+      <button data-modal-target="#modal">Open Modal</button>
+      <div class="modal" id="modal">
+        <div class="modal-header">
+          <div class="title">Example Modal</div>
+          <div data-close-button class="close-button">&times;</div>
+        </div>
+        <div class="modal-body">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ipsum asperiores laboriosam placeat aut saepe, voluptates neque harum. Dignissimos iure dolor sint cupiditate maxime assumenda earum eligendi doloremque voluptates praesentium!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ipsum asperiores laboriosam placeat aut saepe, voluptates neque harum. Dignissimos iure dolor sint cupiditate maxime assumenda earum eligendi doloremque voluptates praesentium!
+        </div>
+      </div>
+      <div id="modal-overlay"></div>
+     </section>
+  </body>
+</html>
+
+<?php
+  require 'footer.php';
+?>
+
