bb75dc18db991e915fb7547717b25abe2a32f080
Vergil Kelley <verg_1@yahoo.com>
2020-07-25 15:28:03 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+echo '<footer>
+<h2>© 2020 Website by <a href="https://vergildkelley.com/" target="_blank">Vergil D. Kelley Web Design & Development.</a> All other trademarks, service marks and trade names referenced in this material are the property of their respective owners.</h2>
+</footer>
+</section>
+</body>
+</html>';
+
+?>
