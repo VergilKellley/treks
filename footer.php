f81f4d6f60850c24c5a05e30c17f1e65fc961bec
Vergil Kelley <verg_1@yahoo.com>
2020-08-18 17:53:47 -0700

    added colums to footer

--- footer.php
+++ footer.php
@@ -4,6 +4,15 @@ echo '<section class="footerContainer">
 <div class="footerColumnOne>
 <h2>© 2020 Website by <a href="https://vergildkelley.com/" target="_blank">Vergil D. Kelley Web Design & Development.</a> All other trademarks, service marks and trade names referenced in this material are the property of their respective owners.</h2>
 </div>
+<div class="footerColumnTwo>
+    <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="">Contact</a></li>
+    </ul>
+</div>
+<div class="footerColumnthree">
+<h3>Some Text</h3>
+</div>
 </footer>
 </section>
 </body>
